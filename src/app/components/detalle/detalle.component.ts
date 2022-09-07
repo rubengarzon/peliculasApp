c4ae4ffa6280d6fab351d0786f76759b0e66c5dd
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-09-07 11:40:33 +0200

    detalle de la pelicula

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,4 +1,7 @@
+import { ModalController } from '@ionic/angular';
 import { Component, Input, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+import { Detalle, Cast } from '../../interfaces/index';
 
 @Component({
   selector: 'app-detalle',
@@ -7,7 +10,32 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DetalleComponent implements OnInit {
   @Input() id;
-  constructor() {}
+  detalle: Detalle = {};
+  actores: Cast[] = [];
+  oculto = 150;
 
-  ngOnInit() {}
+  slideOptActores = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: -5,
+  };
+
+  constructor(
+    private moviesService: MoviesService,
+    private modalCtrl: ModalController
+  ) {}
+
+  ngOnInit() {
+    this.moviesService.getMovieDetail(this.id).subscribe((resp) => {
+      this.detalle = resp;
+    });
+
+    this.moviesService.getActoresPelicula(this.id).subscribe((resp) => {
+      this.actores = resp.cast;
+    });
+  }
+
+  volver() {
+    this.modalCtrl.dismiss();
+  }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -2,6 +2,7 @@ import { environment } from './../../environments/environment';
 import { RespuestaMDB } from './../interfaces/index';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Detalle, RespuestaCredits } from '../interfaces/index';
 
 const URL = environment.url;
 const apiKey = environment.apiKey;
@@ -16,6 +17,7 @@ export class MoviesService {
 
   ejecutarQuery<T>(query: string) {
     query = `${URL}${query}?api_key=${apiKey}&language=es&include_image_language=es${this.page}`;
+    console.log(query);
     return this.http.get<T>(query);
   }
 
@@ -34,5 +36,15 @@ export class MoviesService {
     const query = '/movie/upcoming';
     this.page = '&page=' + this.upcomingPage;
     return this.ejecutarQuery<RespuestaMDB>(query);
+  }
+
+  getMovieDetail(id: number) {
+    const query = `/movie/${id}`;
+    return this.ejecutarQuery<Detalle>(query);
+  }
+
+  getActoresPelicula(id: number) {
+    const query = `/movie/${id}/credits`;
+    return this.ejecutarQuery<RespuestaCredits>(query);
   }
 }
