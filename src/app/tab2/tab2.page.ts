c923d526a38aa4baf76c93989f415fac4603db75
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-09-08 11:36:10 +0200

    diseño pagina busqueda

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
+  textoBuscar = '';
+  ideas: string[] = [
+    'El señor de los anillos',
+    'Harry Potter',
+    'Star Wars',
+    'El Hobbit',
+  ];
   constructor() {}
-
+  buscar(event) {
+    const valor = event.detail.value;
+  }
 }
